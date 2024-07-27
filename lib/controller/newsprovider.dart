4aec9194ebf0b8322bc2c55320b336553155fef7
haadiyaa <haadiyact@gmail.com>
2024-07-27 15:19:45 +0530

    news model

--- lib/controller/newsprovider.dart
+++ lib/controller/newsprovider.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:machinetest_newsapp/secrets/secrets.dart';
 class NewsProvider extends ChangeNotifier {
-    
+    bool isLoading = false;
+    String error='';
+    void fetchNews(String city)async {
+      isLoading=true;
+      error="";
+      try {
+        final response= await http.get(Uri.parse('${Secrets.baseUrl}${Secrets.countryUrl}in${Secrets.apikeyUrl}${Secrets.key}'));
+      } catch (e) {
+        
+      }
+    }
 }
