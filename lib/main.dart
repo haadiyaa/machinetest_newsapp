6e222e60fdb0bfbea8a5ce3d42b4255fba2ef643
haadiyaa <haadiyact@gmail.com>
2024-07-27 11:52:33 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:machinetest_newsapp/firebase_options.dart';
 import 'package:machinetest_newsapp/utils/constants.dart';
 import 'package:machinetest_newsapp/view/authenticationview/view/loginpage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,9 +18,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size=MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return ScreenUtilInit(
-      designSize: Size(size.width,size.height),
+      designSize: Size(size.width, size.height),
       builder: (context, child) => MaterialApp(
         theme: ThemeData(
           scaffoldBackgroundColor: Constants.bgColor,
